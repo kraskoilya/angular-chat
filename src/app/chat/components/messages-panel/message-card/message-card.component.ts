f7d885ce1a8cf1d6379d60526503aa7a9b14045a
ilya_kracko <ilya.kracko@gmail.com>
2021-12-10 11:29:42 +0300

    add messages

--- src/app/chat/components/messages-panel/message-card/message-card.component.ts
+++ src/app/chat/components/messages-panel/message-card/message-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Message } from 'src/app/chat/models/message';
 
+type PositionType = 'left' | 'right';
+
 @Component({
   selector: 'app-message-card',
   templateUrl: './message-card.component.html',
@@ -8,6 +10,7 @@ import { Message } from 'src/app/chat/models/message';
 })
 export class MessageCardComponent implements OnInit {
   @Input() item!: Message;
+  @Input() position!: PositionType;
 
   constructor() {}
 
