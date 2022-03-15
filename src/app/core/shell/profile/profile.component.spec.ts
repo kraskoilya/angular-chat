f56fdbb22107b07d56eea0c2a0f93c1fc9ea36c2
Ilya Krasko <ilya.kracko@gmail.com>
2022-03-15 11:44:29 +0300

    some tests

--- src/app/core/shell/profile/profile.component.spec.ts
+++ src/app/core/shell/profile/profile.component.spec.ts
@@ -1,64 +1,97 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { UserService } from '../../services/user.service';
-// import { ProfileComponent } from './profile.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzModulesModule } from 'src/app/shared/nz-modules/nz-modules.module';
+import { AUTH_TOKEN_STORAGE_KEY } from '../../mics/injection-tokens';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { ProfileComponent } from './profile.component';
 
-// describe('ProfileComponent', () => {
-//   let component: ProfileComponent;
-//   let fixture: ComponentFixture<ProfileComponent>;
-//   let MockUserService: Partial<UserService>;
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
 
-//   let mockUser = {
-//     name: 'krasko ilya',
-//     first_name: 'ilya',
-//     last_name: 'krasko',
-//     avatar: '',
-//     createdAt: '11.11.11',
-//     updatedAt: '',
-//     email: 'email@email.com',
-//     id: 1,
-//     update() {},
-//   };
+  const mockUser = {
+    id: '1',
+    name: 'krasko ilya',
+    first_name: 'ilya',
+    last_name: 'krasko',
+    avatar: '',
+    createdAt: '11.11.11',
+    updatedAt: '',
+    email: 'email@email.com',
+    update() {},
+  };
 
-//   MockUserService = {
-//     user: {
-//       name: 'krasko ilya',
-//       first_name: 'ilya',
-//       last_name: 'krasko',
-//       avatar: '',
-//       createdAt: '11.11.11',
-//       updatedAt: '',
-//       email: 'email@email.com',
-//       id: 1,
+  let userServiceSpy = jasmine.createSpyObj('UserService', ['user']);
 
-//       update() {},
-//     },
-//   };
+  let authServiceSpy = jasmine.createSpyObj('AuthService', ['logout']);
+  authServiceSpy.logout.and.returnValue();
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ProfileComponent],
-//       providers: [
-//         {
-//           provide: UserService,
-//           useValue: MockUserService,
-//         },
-//       ],
-//     }).compileComponents();
-//     MockUserService = TestBed.inject(UserService);
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        NzModulesModule,
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        NzDropDownModule,
+      ],
+      declarations: [ProfileComponent],
+      providers: [
+        NzModalService,
+        AuthService,
+        {
+          provide: AuthService,
+          useValue: authServiceSpy,
+        },
+        {
+          provide: UserService,
+          useValue: userServiceSpy,
+        },
+        {
+          provide: AUTH_TOKEN_STORAGE_KEY,
+          useValue: 'my_test_chat',
+        },
+      ],
+    }).compileComponents();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ProfileComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfileComponent);
+    component = fixture.componentInstance;
 
-//   it('should create component', () => {
-//     expect(component).toBeTruthy();
-//   });
+    component.ngOnInit();
 
-//   it(`should have user`, () => {
-//     component = fixture.componentInstance;
-//     expect(component.user).toEqual(mockUser);
-//   });
-// });
+    userServiceSpy.user = mockUser;
+    fixture.detectChanges();
+  });
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have user`, fakeAsync((done: DoneFn) => {
+    tick();
+    expect(component.user).toEqual(mockUser);
+  }));
+
+  it('should open "Update information" modal', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should logout user`, fakeAsync((done: DoneFn) => {
+    authServiceSpy.logout();
+    tick();
+    expect(authServiceSpy.logout).toHaveBeenCalled();
+    expect(authServiceSpy.logout.calls.count()).toBe(1);
+  }));
+});
--- src/app/shared/modals/chat-create/chat-create.component.spec.ts
+++ src/app/shared/modals/chat-create/chat-create.component.spec.ts
@@ -1,23 +1,157 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
+import { of } from 'rxjs';
+import { ChatsService } from 'src/app/chat/services/chats.service';
+import { AUTH_TOKEN_STORAGE_KEY } from 'src/app/core/mics/injection-tokens';
+import { User } from 'src/app/core/models/user';
+import { UserService } from 'src/app/core/services/user.service';
+import { NzModulesModule } from '../../nz-modules/nz-modules.module';
 import { ChatCreateComponent } from './chat-create.component';
+
+const mockUsers: User[] = [
+  {
+    id: '1',
+    email: 'email1@email.com',
+    first_name: 'Ilya',
+    last_name: 'Krasko',
+    createdAt: '',
+    updatedAt: '',
+    name: 'Ilya Krasko',
+
+    update() {},
+  },
+  {
+    id: '2',
+    email: 'email@email.com',
+    first_name: 'Ilya',
+    last_name: 'Krasko',
+    createdAt: '',
+    updatedAt: '',
+    name: 'Ilya Krasko',
+
+    update() {},
+  },
+];
 
 describe('ChatCreateComponent', () => {
   let component: ChatCreateComponent;
   let fixture: ComponentFixture<ChatCreateComponent>;
+  let fb: FormBuilder;
+
+  let userServiceSpy = jasmine.createSpyObj('UserService', ['getUsers']);
+  userServiceSpy.getUsers.and.returnValue(of(mockUsers));
+
+  let chatsServiceSpy = jasmine.createSpyObj('ChatsService', ['createItem']);
+  chatsServiceSpy.createItem.and.returnValue(of());
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        NzModulesModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+      ],
       declarations: [ChatCreateComponent],
-    }).compileComponents();
+      providers: [
+        { provide: UserService, useValue: userServiceSpy },
+        { provide: ChatsService, useValue: chatsServiceSpy },
+        FormBuilder,
+        {
+          provide: NzModalRef,
+          useFactory: (modalSvc: NzModalService) =>
+            modalSvc.create({
+              nzClosable: false,
+              nzContent: ChatCreateComponent,
+            }),
+          deps: [NzModalService],
+        },
+        {
+          provide: AUTH_TOKEN_STORAGE_KEY,
+          useValue: 'my_test_chat',
+        },
+      ],
+    })
+      .overrideModule(BrowserTestingModule, {
+        set: { entryComponents: [ChatCreateComponent] },
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ChatCreateComponent);
     component = fixture.componentInstance;
+
+    fb = TestBed.inject(FormBuilder);
+
+    component.form = fb.group({
+      title: [null, Validators.required],
+      users: [null, Validators.required],
+    });
+
+    component.ngOnInit();
+
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should getUsers to create chat', () => {
+    expect(userServiceSpy.getUsers).toHaveBeenCalled();
+
+    expect(component.users?.length).toBe(2);
+  });
+
+  it('should be valid if form value is VALID', () => {
+    component.form.setValue({
+      title: 'Ilya',
+      users: mockUsers,
+    });
+
+    expect(component.form.valid).toEqual(true);
+  });
+
+  it('should be valid if form value is INVALID', () => {
+    component.form.setValue({
+      title: 'Ilya',
+      users: null,
+    });
+
+    expect(component.form.invalid).toEqual(true);
+  });
+
+  it('should allow chat to create', () => {
+    const formData = {
+      title: 'Ilya',
+      users: mockUsers.map((el) => el.id),
+    };
+    component.form.setValue(formData);
+    expect(component.form.errors).toBeNull();
+    expect(component.form.valid).toEqual(true);
+
+    component.send();
+
+    const body = {
+      ...formData,
+      users: formData.users.map((el) => {
+        return {
+          id: el,
+        };
+      }),
+    };
+
+    expect(chatsServiceSpy.createItem).toHaveBeenCalledWith(
+      body,
+      component.form
+    );
+    expect(component.errorMessage).toBeNull();
+  });
 });
